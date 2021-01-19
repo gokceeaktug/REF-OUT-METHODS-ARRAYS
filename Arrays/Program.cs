4febf5806b62eacbed6b434637e842bd537bace4
Gökçe Aktuğ <gokceeaktug@gmail.com>
2021-01-20 02:02:33 +0300

    first commit

---
+++ Arrays/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string student1 = "Engin";
+            string student2 = "Derin";
+            string student3 = "Salih";
+
+            string[] students = new string[3];
+            students[0] = "Engin";
+            students[1] = "Derin";
+            students[2] = "Salih";
+
+            string[] students2 = new[] { "Ayşe", "Fatma", "Hayriye" };
+          
+
+            foreach(var student in students)
+            {
+
+                Console.WriteLine(student);
+            }
+            foreach(var students1 in students2)
+            {
+                Console.WriteLine(students1);
+            }
+            Console.WriteLine();
+            //----OUTPUT----//
+            //Engin
+            //Derin
+            //Salih
+
+
+            //-----OUTPUT---//
+            //Ayşe
+            //Fatma
+            //Hayriye
+
+        }
+    }
+}
---
+++ Methodss/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace Methodss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 20;
+            int number2 = 100;
+            int result2 = Add3(ref number1,number2);
+            int number3;
+            int number4 = 100;
+            int result3 = Add4(out number3, number4);
+
+            Console.WriteLine(result2);
+            Console.WriteLine(number1);
+            Console.WriteLine(result3);
+            Console.WriteLine(number3);
+            Console.WriteLine(Multiply(2, 4));
+            Console.WriteLine(Multiply(2, 4,5));
+            Console.WriteLine(Add5(1, 2, 3, 4, 5, 6));
+            Console.ReadLine();
+        }
+        static void Add()
+        {
+            Console.WriteLine("Added");
+        }
+        static int Add2(int number1=20,int number2=30)
+        {
+            var result = number1 + number2;
+            return result;
+        }
+        static int Add3(ref int number1,int number2)
+        {
+            number1 = 30;
+            return number1 + number2;
+        }
+        static int Add4(out int number3,int number4)
+        {
+            number3 = 20;
+            return number3 + number4;
+        }
+        static int Multiply(int number5,int number6)
+        {
+            return number5 * number6;
+
+        }
+        static int Multiply(int number5, int number6,int number7)
+        {
+            return number5 * number6* number7;
+
+        }
+        static int Add5(params int[] numbers)
+        {
+            return numbers.Sum();
+
+          
+        }
+    }
+}
